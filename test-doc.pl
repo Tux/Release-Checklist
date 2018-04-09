580599f33089b1dff29800590a1d602d6e8cf126
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2018-04-09 17:06:42 +0200

    Also test with Text::Markdown::Hoedown if available

--- test-doc.pl
+++ test-doc.pl
@@ -3,18 +3,36 @@
 use strict;
 use warnings;
 
-our $VERSION = "0.01 - 20180330";
+use Test::More;
+
+our $VERSION = "0.02 - 20180409";
 
 -d ".git" or exit 0;
 
-eval "use Text::Markdown qw( markdown )" or exit 0;
-$@ and exit 0;
+my $tmd = eval { require Text::Markdown;          1; };
+my $tmh = eval { require Text::Markdown::Hoedown;
+    Text::Markdown::Hoedown::HOEDOWN_EXT_AUTOLINK ()
+  | Text::Markdown::Hoedown::HOEDOWN_EXT_STRIKETHROUGH ()
+  | Text::Markdown::Hoedown::HOEDOWN_EXT_UNDERLINE ()
+  | Text::Markdown::Hoedown::HOEDOWN_EXT_HIGHLIGHT ()
+  | Text::Markdown::Hoedown::HOEDOWN_EXT_NO_INTRA_EMPHASIS ();
+  };
 
-foreach my $md (sort glob "*.md") {
-    print "$md\n";
-    open my $fh, "<", $md or next;
-    local $/;
+foreach my $mdf (sort glob "*.md") {
+    ok (my $md = do {
+	open my $fh, "<", $mdf or next;
+	local $/;
+	<$fh>;
+	}, $mdf);
     # No idea if this really *checks* if the .md is valid
-    markdown (<$fh>);
-    close $fh;
+    if ($tmd) {
+	my $html = Text::Markdown::markdown          ($md);
+	like ($html, qr{^<\w+}, "Got html with Text::Markdown          for $mdf");
+	}
+    if ($tmh) {
+	my $html = Text::Markdown::Hoedown::markdown ($md, extensions => $tmh);
+	like ($html, qr{^<\w+}, "Got html with Text::Markdown::Hoedown for $mdf");
+	}
     }
+
+done_testing ();
