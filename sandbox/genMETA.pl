ade75aacbd5a2e8c460198bc2f0fb05a0a2f932b
H.Merijn Brand - Tux <linux@tux.freedom.nl>
2023-10-12 15:32:44 +0200

    Repair e-mail

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -44,7 +44,7 @@ version:                VERSION
 abstract:               A QA checklist for CPAN releases
 license:                perl
 author:                 
-  - H.Merijn Brand <hmbrand.org>
+  - H.Merijn Brand <hmbrand@cpan.org>
 generated_by:           Author
 distribution_type:      module
 release_status:         stable
